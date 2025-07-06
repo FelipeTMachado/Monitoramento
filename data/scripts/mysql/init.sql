a62f4bda08bbdc386bb47778302dac35a4024299
felipe caue machado <felipe.caue.trabalho@gmail.com>
2025-07-06 05:21:57 -0300

    Criar scripts/funções de conexão em PHP, um para o MySQL e outro para o MongoDB

--- data/scripts/mysql/init.sql
+++ data/scripts/mysql/init.sql
@@ -1,17 +1,16 @@
 CREATE TABLE usuario (
     id                    INT AUTO_INCREMENT PRIMARY KEY,                                  
-    name                  VARCHAR(255) NOT NULL,                                           
+    nome                  VARCHAR(255) NOT NULL,                                           
     email                 VARCHAR(255) NOT NULL UNIQUE,                                    
     senha                 VARCHAR(255) NOT NULL,                                      
     data_criacao          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,                             
     data_atualizacao      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-
 CREATE TABLE monitor (
     id                    INT AUTO_INCREMENT PRIMARY KEY,
     id_usuario            INT NOT NULL,
-    name                  VARCHAR(255) NOT NULL, 
+    nome                  VARCHAR(255) NOT NULL, 
 
     tipo_monitor          ENUM('HTTP', 'PING', 'PORT') NOT NULL, 
     alvo                  VARCHAR(2048) NOT NULL,                              
@@ -27,4 +26,12 @@ CREATE TABLE monitor (
     
     FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
 );
+
+INSERT INTO usuario (id, nome, email, senha) VALUES (1, 'Felipe Caue Machado', 'fmachad6@gmail.com',  '1234');
+
+INSERT INTO monitor (id_usuario, nome, tipo_monitor, alvo, porta, intervalo_verificacao, esta_ativo, ultimo_status, ultima_verificacao) VALUES (1, 'Monitor do Google', 'HTTP', 'https://www.google.com', NULL, 300, TRUE, 'PENDING', NULL);
+INSERT INTO monitor (id_usuario, nome, tipo_monitor, alvo, porta, intervalo_verificacao, esta_ativo, ultimo_status, ultima_verificacao) VALUES (1, 'Ping para Google DNS', 'PING', '8.8.8.8', NULL, 60, TRUE, 'PENDING', NULL);
+INSERT INTO monitor (id_usuario, nome, tipo_monitor, alvo, porta, intervalo_verificacao, esta_ativo, ultimo_status, ultima_verificacao) VALUES (1, 'Porta SSH do Servidor Fabtec', 'PORT', '192.168.0.10', 22, 120, TRUE, 'PENDING', NULL);
+INSERT INTO monitor (id_usuario, nome, tipo_monitor, alvo, porta, intervalo_verificacao, esta_ativo, ultimo_status, ultima_verificacao) VALUES (1, 'Site Institucional IFC', 'HTTP', 'https://ifc-riodosul.edu.br', NULL, 180, TRUE, 'PENDING', NULL);
+
 
